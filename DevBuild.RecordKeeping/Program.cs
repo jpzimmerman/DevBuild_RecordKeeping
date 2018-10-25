40844e0dd1e9a877cc904616ec52999e51454a9c
Josh Zimmerman <JoshuaZimmerman@quickenloans.com>
2018-10-25 11:07:43 -0400

    Add project files.

---
+++ DevBuild.RecordKeeping/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBuild.RecordKeeping {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
